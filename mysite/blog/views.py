dbb836e1b743333b40ac5aeb1bd5ad0da57f5147
SashDu <sashadubina1996@gmail.com>
2023-07-01 02:31:01 +0300

    added forms and created to send Email in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,8 @@ from django.http import Http404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
+from django. views.generic import ListView
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -33,3 +35,40 @@ def post_detail(request, year, month, day, post):
                   {'post': post})
 
 
+class PostListView(ListView):
+    """
+    Alternative view of the list of posts
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    #Extract a post by ID
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        #The form has been submitted for processing
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #The form fields have been successfully validated
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'BITW120496@gamil.com',
+                      [cd['to']])
+            sent = True
+
+            #... send an email
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})

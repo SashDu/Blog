f9981dc47dab26a25081d8719f358e69531b2df6
SashDu <sashadubina1996@gmail.com>
2023-07-03 02:07:03 +0300

    added system of coments, added template fixed some bugs in admin panels

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,9 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
 from django. views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -31,9 +32,15 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    #List active comments to this post
+    comments = post.comments.filter(active=True)
+    #Form for coment to users
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
 
 class PostListView(ListView):
@@ -73,3 +80,24 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    #The comment was send
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        #Create object of class Comment, not save it in DB
+        comment = form.save(commit=False)
+        # Assign post to comment
+        comment.post = post
+        # Save comment in DB
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                  'form': form,
+                  'comment': comment})
